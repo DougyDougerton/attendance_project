3a64e2c4dd643c6ab18895d0c656dccb85df64d4
Douglas Spicer <doug@Douglass-MacBook-Air.local>
2024-08-26 13:04:13 -0600

    Updated Code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes.js');
 const studentRoutes = require('./routes/studentRoutes.js');
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 //Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => { console.log('아싸! You connected to MongoDB Database! :)') })
-.catch((err) => { console.log(`안 좋다! Server is broken :(): ${err}`) });
+.catch((err) => { console.log(`안 좋다! Can't connect to the server! :(: ${err}`) });
 
 //View engines
 app.set('view engine', 'ejs');
@@ -18,12 +19,15 @@ app.set('views', './views');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', authRoutes);
 app.use('/', studentRoutes);
+
 app.use((err, res, req, next) =>{
     console.error(err.stack);
-    res.status(500).send('Shit\'s fucked >:(');
+    res.status(500).send('Server is broken >:(');
     next();
 });
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,47 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const AttendanceManager = require('../models/attendanceManager.js');
 require('dotenv').config();
+
+exports.register = async(req,res) => {
+
+    const {email, password, confirmPassword} = req.body;
+    
+    try{
+        const existingUser = await AttendanceManager.findOne({email});
+        console.log(email);
+        console.log(password);
+        console.log(confirmPassword);
+        if(existingUser){
+            return res.status(400).send("Username already exists. Please choose another username :)");
+
+
+
+        }
+        
+        if(password !== confirmPassword){
+            return res.status(400).send("Passwords do not match. :( Sad Panda 🐼");
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new AttendanceManager({
+            email,
+            password: hashPassword
+        });
+
+        await newUser.save();
+
+        res.redirect('/login');
+
+    }catch(error){
+
+    }
+
+
+
+
+
+
+}
+
