80f758699e18f32cb3a30b3aa4b86d431e41ba6e
Douglas Spicer <doug@Douglass-MacBook-Air.local>
2024-08-27 10:07:22 -0600

    Updated code

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,45 +3,75 @@ const jwt = require('jsonwebtoken');
 const AttendanceManager = require('../models/attendanceManager.js');
 require('dotenv').config();
 
-exports.register = async(req,res) => {
 
-    const {email, password, confirmPassword} = req.body;
-    
-    try{
-        const existingUser = await AttendanceManager.findOne({email});
-        console.log(email);
-        console.log(password);
-        console.log(confirmPassword);
-        if(existingUser){
-            return res.status(400).send("Username already exists. Please choose another username :)");
+exports.login = async(req,res) => {
 
-
-
-        }
-        
-        if(password !== confirmPassword){
-            return res.status(400).send("Passwords do not match. :( Sad Panda 🐼");
-        }
-
-        const hashPassword = await bcrypt.hash(password, 10);
-
-        const newUser = new AttendanceManager({
-            email,
-            password: hashPassword
-        });
-
-        await newUser.save();
-
-        res.redirect('/login');
-
-    }catch(error){
-
-    }
-
-
-
-
-
-
+    const {email, password} = req.body;
 }
+    try{
+        const user = await AttendanceManager.findOne({email});
+    
+        if(!user){
+            return res.status(401).send("Invaild username!");
 
+            //Verify the password using bcrypt
+            const result = await bcrypt.compare(password, user.password);
+
+            if(!result){
+                return res.status(401).send('It is not vaild!');
+            }
+
+            //Generate the JWT
+            const token = jwt.sign({id:user._id.toString()}, 'secret_key', {expiresIn: '5m'});
+
+           //Create a cookie and place JWT/token inside it
+            res.cookie('jwt', token, {maxAge: 5 * 60 * 1000, http: true});
+
+            res.redirect('attendance');
+
+            
+             
+         }}
+        
+    
+        
+
+
+// exports.register = async(req,res) => {
+
+//     const {email, password, confirmPassword} = req.body;
+    
+//     try{
+//         const existingUser = await AttendanceManager.findOne({email});
+
+//         if(existingUser){
+//             return res.status(400).send("Username already exists. Please choose another username :)");
+
+
+
+//         }
+        
+//         if(password !== confirmPassword){
+//             return res.status(400).send("Passwords do not match. :( Sad Panda 🐼");
+//         }
+
+//         const hashPassword = await bcrypt.hash(password, 10);
+
+//         const newUser = new AttendanceManager({
+//             email,
+//             password: hashPassword
+//         });
+
+//         await newUser.save();
+
+//         res.redirect('/login');
+
+//     }catch(error){
+//         res.status(500).send('Server is broken. Hold tight!')
+//     }
+
+
+// }
+catch (error) {
+    res.status(500).send('Server has a silly brain!');
+}
